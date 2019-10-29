cdc3fb1ca753da144ef55813cfa23a6528a63762
Ben <benhacker95@gmail.com>
2019-10-29 15:58:51 -0500

    Header component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Components/Header';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h2>Team Singularity</h2>
       </header>
+      <Header>
+      </Header>
     </div>
   );
 }
