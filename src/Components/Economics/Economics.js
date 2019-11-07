92702f5aee5418119e6c1cd7daa9bdb2b0280597
Ben <benhacker95@gmail.com>
2019-11-07 15:51:00 -0600

    Updated Economics page with new flashcards

--- src/Components/Economics/Economics.js
+++ src/Components/Economics/Economics.js
@@ -6,15 +6,7 @@ import Header from '../Header/Header';
 import {PropTypes} from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Flashcard from '../Card/Flashcard';
 
 const styles = theme => ({
     card: {
@@ -62,58 +54,22 @@ class Economics extends Component{
         const { classes } = this.props;
         return (
             <div className='content'>
+            <div>
                 <Header history={this.props.history}/>
-                <Card className={classes.card} display='inline'>
-                    <CardContent>
-                        <Typography>
-                            <h3>Introduction</h3>
-                        </Typography>
-                    </CardContent>
-                <CardContent>
-                <Typography component="p">
-                    </Typography>
-                    <hr/>
-                </CardContent>
-                <IconButton
-                    onClick={this.handleExpandClick}
-                    aria-label="show more"
-                    >
-                    <ExpandMoreIcon />
-                </IconButton>
-                <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-                    <CardContent>
-                    <Typography paragraph variant="body2">
-                    <h3>As humanity stands on the brink of a technology activated information revolution, the scale, choice and effort of the impact of intelligence evolution in machines is distinct anything humanity has qualified before. As a result, the speed at which the ideas, revolutions and discoveries are emerging on the back of artificial intelligence has no historical example and is essentially troublemaking everything in the human ecosystem. Artificial intelligence continues to develop at its present dizzying rate, the singularity can come about in the central of the current century.</h3>
-                    </Typography>
-                    </CardContent>
-                </Collapse>
-                </Card>
-                <Card className={classes.card} display='inline'>
-                    <CardContent>
-                        <Typography>
-                            <h3>Information Evolution</h3>
-                        </Typography>
-                    </CardContent>
-                <CardContent>
-                <Typography component="p">
-                    </Typography>
-                    <hr/>
-                </CardContent>
-                <IconButton
-                    onClick={this.handleExpandClick}
-                    aria-label="show more"
-                    >
-                    <ExpandMoreIcon />
-                </IconButton>
-                <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-                    <CardContent>
-                    <Typography paragraph variant="body2">
-                    <h3>The success of the quickly progressing information age is based on data, and data and information are all around us. When we assess the information and intelligence development in machines, we understand that it has inaccurate human life in important originations in how we make digital data and information, how we capture and gather the digital data, how we stock it, how we save and matching the information thereby clouding the limits of languages, time zones, politics, philosophy, race, religion and culture.</h3>
-                    </Typography>
-                    </CardContent>
-                </Collapse>
-                </Card>
             </div>
+            <Flashcard
+                title = {'Overview of Economic Issues with the Singularity'}
+                content = {'The success of the quickly progressing information age is based on data, and data and information are all around us. When we assess the information and intelligence development in machines, we understand that it has inaccurate human life in important originations in how we make digital data and information, how we capture and gather the digital data, how we stock it, how we save and matching the information thereby clouding the limits of languages, time zones, politics, philosophy, race, religion and culture.'}
+            />
+            <Flashcard
+                title = {'The Fall of Capitalism?'}
+                content = {'Current information revolution, as the machine learning systems improve quickly, the power and promise of software that learn by example, patterns and models seems immense. Along with the imposing advances in software development, there also now seems to be a parallel evolution in computer hardware to recover machine intelligence capabilities. This is clearly noticeable in the growth of ongoing efforts towards developing systems on a chip, focused on restructuring more efficient, lower energy overriding microprocessor chips that imitator the human brain circuitry. As these rapidly evolving neuromorphic chips are being planned to process human physical data such as images, smell and sound and to answer to changes in that data in ways not exactly programmed, a lot is expected to change for machine intelligence and artificial intelligence evolution.'}
+            />
+            <Flashcard
+                title={'So What Happens When We Have No Money?'}
+                content={'There are those who trust that to overcome artificial super intelligence, developing methods can be used to improve human intelligence and make a superhuman with super intelligence. Although in theory, creating a superhuman with super intelligence is likely thinkable over intelligence intensification of human brain or intelligence increase the truth is evolution of human brain and human intelligence is a very composite attempt with too many unknowns, dependencies and variables.'}
+            />
+        </div>
         )
     }
 }
